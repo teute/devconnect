076d17843c4936165c02c6c88f03b718c861b482
Salieu Kamara <saluege@gmail.com>
2020-05-09 14:34:24 +0100

    Implement alert reducer, action & types

---
+++ client/src/actions/alert.js
@@ -0,0 +1,10 @@
+import { v4 as uuid } from 'uuid';
+import { SET_ALERT, REMOVE_ALERT } from './types';
+
+export const setAlert = (msg, alertType) => {
+  const id = uuid();
+  dispatch({
+    type: SET_ALERT,
+    payload: { msg, alertType, id },
+  });
+};
